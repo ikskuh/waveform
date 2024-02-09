b21ee2a5ddb69c44ac95a9da535d1717b3ece223
Felix "xq" Queißner <git@random-projects.net>
2024-02-09 15:47:48 +0100

    Adds GitHub CI

--- build.zig
+++ build.zig
@@ -3,7 +3,6 @@ const std = @import("std");
 pub fn build(b: *std.build.Builder) void {
     // steps:
     const run_step = b.step("run", "Run the app");
-    const test_step = b.step("test", "Run unit tests");
 
     // options:
     const target = b.standardTargetOptions(.{});
@@ -32,13 +31,4 @@ pub fn build(b: *std.build.Builder) void {
         run_cmd.addArgs(args);
     }
     run_step.dependOn(&run_cmd.step);
-
-    // Unit tests:
-
-    const exe_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-    test_step.dependOn(&b.addRunArtifact(exe_tests).step);
 }
